11241557c1a521c5bf49746c4d2332c764f94d54
戸村愛有 <tomura.airi00@gmail.com>
2025-12-10 11:02:20 +0900

    boardモデル作成

--- myapp/db/seeds.rb
+++ myapp/db/seeds.rb
@@ -7,3 +7,20 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+john = User.create!(email: 'john00@example.com', password: 'password')
+emily = User.create!(email: 'emily00@example.com', password: 'password')
+
+5.times do
+    john.board.create(
+        tittle: Faker::Lorem.sentence(word_count: 5),
+        content: Faker::Lorem.sentence(word_count: 100)
+    )
+end
+
+5.times do
+    emily.board.create(
+        tittle: Faker::Lorem.sentence(word_count: 5),
+        content: Faker::Lorem.sentence(word_count: 100)
+    )
+end
+
